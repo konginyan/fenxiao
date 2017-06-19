1a4f4f7dc7d82407cdb1fa54952ad40e19481053
wangqinzhou <694804801@qq.com>
2017-06-19 16:21:38 +0800

    add students

--- src/views/components/rate.vue
+++ src/views/components/rate.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="rate">
-		<div v-for="item in max" >
+		<div v-for="item in max">
 			<icon v-if="item<=value" :size="size" @click="selectValue(item)" name="icon-star" class="star" :style="{'color':color}"></icon>
 			<icon  v-if="item>value" @click="selectValue(item)" name="icon-star" :size="size" class="star" :style="{'color':'#dedede'}"></icon>
 		</div>
+		<!-- <text>{{value}}</text> -->
 	</div>
 </template>
 
@@ -46,13 +47,9 @@
 					return;
 				}
 				this.value = value;
+				// buiweex.toast(this.value);
 				this.$emit('change', value);
 			}
-		},
-		computed :{
-			
-
-			
 		}
 		
 	}
