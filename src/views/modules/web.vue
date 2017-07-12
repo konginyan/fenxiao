30815f35f99dd83cdbb8b9670f2aac3edb5c86ce
konginyan <1223319277@qq.com>
2017-07-12 16:37:19 +0800

    little commit

--- src/views/modules/web.vue
+++ src/views/modules/web.vue
@@ -1,16 +1,18 @@
 <template>
 	<div>
 		<bui-header
-					:title="name"
-					:leftItem="leftItem"
-					@leftClick="back">
-					
-			</bui-header>
-		<web ref="webview" :src="url" style="flex:1;width:750px;"></web>
+				:title="name"
+				:leftItem="leftItem"
+				@leftClick="back">
+				
+		</bui-header>
+		<web ref="webview" :src="url" style="flex:1;width:750px;" @pagefinish="onfinish" @error="onfinish"></web>
+		<progress ref="progress" :marginTop="getTop()"></progress>
 	</div>
 </template>
 <script>
 import buiweex from "../../js/buiweex.js";
+import {extend} from "../../js/tool.js";
 var globalEvent = weex.requireModule('globalEvent');
 const webview = weex.requireModule('webview');
 	export default {
@@ -27,10 +29,16 @@ const webview = weex.requireModule('webview');
 			back () {
 					buiweex.pop();
 			},
+			getTop (){
+				if(weex.config.env.platform==='iOS') return '117px';
+				else return '100px';
+			},
+			"onfinish": function(){
+				extend(this.$refs.progress,{id:'progress', width:750, duration:1000, opacity:'0'});
+			}
 		},
 
 		mounted (){
-
 			globalEvent.addEventListener("androidback", function (e){
 				buiweex.pop();
 			});
